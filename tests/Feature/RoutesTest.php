ab0c99081ba2b1a378f5b09bfe05bb989736989c
oscarSM13 <oscar.ss.mm@gmail.com>
2018-06-10 22:47:47 +0200

    Testing

---
+++ tests/Feature/RoutesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RoutesTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testHome()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testPromociones()
+    {
+        $response = $this->get('/promociones');
+
+        $response->assertStatus(200);
+    }
+
+    public function testLogin()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function testRegister()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/UsersTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Code;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
+
+class UsersTest extends TestCase{
+
+    use DatabaseTransactions;
+	use InteractsWithDatabase;
+
+	public function testCanLogin()
+	{
+		$user = factory(User::class)->create();
+
+		$response = $this->post('/login', [
+			'email' => $user->email,
+			'password' => 'secret',
+		]);
+
+		$this->assertAuthenticatedAs($user);
+    }
+    
+    public function testCanFollow()
+	{
+		$user = factory(User::class)->create();
+		$code = factory(Code::class)->create();
+
+		$response = $this->actingAs($code)->post($code->user_id.'/promociones');
+
+		$this->assertDatabaseHas('codes', [
+			'user_id' => $user->id,
+		]);
+	}
+}
