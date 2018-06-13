9a5462eff159975e7bc96a0d64efea3f42f744cd
Marc Ramonell Oliver <marc@discubre.com>
2018-06-13 12:27:27 +0200

    Mejoras de frontend

---
+++ resources/assets/js/components/CreateCuponComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col-md-6">
+        <h3>Haga click para obtener una codigo promocional</h3>
+        <button id="btnSave" type="button" class="btn btn-primary">Crear Cupón</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CreateCuponComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/ValidateCuponComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ValidateCuponComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
